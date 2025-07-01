d9c7bc5b7fd44d3981a4d73954a962f0f3b9e151
Xênia Beatriz <56120697+xeniabia@users.noreply.github.com>
2025-07-01 15:14:27 +0000

    baixei o bootstrap

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -5,7 +5,8 @@ function Nav (){
         <li><NavLink to={"/"}>Home</NavLink></li>
         <li><NavLink to={"areamedica"}>Área Médica</NavLink></li>
         <li><NavLink to={"cadastro"}>Cadastro</NavLink></li>
-
+        
         </ul></>)
+        
 }
 export default Nav
