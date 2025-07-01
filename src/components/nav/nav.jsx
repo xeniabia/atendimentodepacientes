06e72605ecd605d344eb900fc97a47adead72f4b
Xênia Beatriz <56120697+xeniabia@users.noreply.github.com>
2025-07-01 15:04:22 +0000

    roteamento de páginas

---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom"
+
+function Nav (){
+    return (<><ul>
+        <li><NavLink to={"/"}>Home</NavLink></li>
+        <li><NavLink to={"areamedica"}>Área Médica</NavLink></li>
+        <li><NavLink to={"cadastro"}>Cadastro</NavLink></li>
+
+        </ul></>)
+}
+export default Nav
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Nav from "../components/nav/nav"
+
+
 function RoutesNav(){
-    return(<h1>Rotas</h1>)
+    return(<BrowserRouter>
+    <Nav/>
+        <Routes>
+            <Route path="/" element={<h1>Home</h1>}/>
+            <Route path="areamedica" element={<h1>Área Médica</h1>}/>
+            <Route path="cadastro"element={<h1>Cadastro</h1>}/>
+        </Routes>
+    </BrowserRouter>)
 }
 
 export default RoutesNav
