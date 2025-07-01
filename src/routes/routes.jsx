dc23dfd215e2ddad43f65586ef6b07056dbb4f60
Ingridoliveira <oligrid20@gmail.com>
2025-07-01 17:43:45 -0300

    roteamento de paginas

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,17 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Nav from "../components/nav/nav"
-import AreaMedica from "../pages/areamedica/areamedica"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "../components/nav/nav.jsx";
+import AreaMedica from "../pages/areamedica/areamedica.jsx";
+import Home from "../pages/home/home.jsx";
+import Cadastro from "../pages/cadastro/cadastro.jsx";
 
-
-function RoutesNav(){
-    return(<BrowserRouter>
-    <Nav/>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="areamedica" element={<AreaMedica/>}/>
-            <Route path="cadastro"element={<Cadastro/>}/>
-        </Routes>
-    </BrowserRouter>)
+function RoutesNav() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="areamedica" element={<AreaMedica/>} />
+        <Route path="cadastro" element={<Cadastro/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default RoutesNav
+export default RoutesNav;
