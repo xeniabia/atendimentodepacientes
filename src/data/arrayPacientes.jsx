7b5625369d8a3d229548837c335da0bce5132e0e
Ingridoliveira <oligrid20@gmail.com>
2025-07-02 00:04:32 -0300

    array teste

---
+++ src/data/arrayPacientes.jsx
@@ -0,0 +1,28 @@
+
+const ArrayPacientes = [
+  [
+    {
+      id: "1ed2e2d9-9709-46e1-ab0d-729e8317269b",
+      name: "Pedro",
+      reason: "dor de barriga",
+      level: 1,
+      status: 0,
+    },
+    {
+      id: "69667215-c8b2-47ca-9cac-a21d54d3f1e2",
+      name: "Paulo",
+      reason: "dor de cabeça",
+      level: 2,
+      status: 0,
+    },
+    {
+      id: "58523a4b-30a2-4bb6-a4a8-8cea9480336c",
+      name: "Alice",
+      reason: "ALERGIA A CAMARÃO",
+      level: 3,
+      status: 0,
+    },
+  ]
+];
+
+export default ArrayPacientes;
