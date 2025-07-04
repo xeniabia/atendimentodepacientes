ab7237cbe626525b1019349fbc0dffcba9fcee43
Ingridoliveira <oligrid20@gmail.com>
2025-07-04 13:14:39 -0300

    refatoramento do botão doctor buton

--- src/components/Butons/DoctorButons.jsx
+++ src/components/Butons/DoctorButons.jsx
@@ -1,31 +1,42 @@
-
+import { useState, useEffect } from "react";
 
 function DoctorButons({ id, onUpdateStatus }) {
+  const storageKey = `cor-button-${id}`;
 
-  
+  const [cor, setCor] = useState(() => {
+    return localStorage.getItem(storageKey) || "btn-primary";
+  });
 
-  const handleClick = (newStatus) => {
-   console.log(`Paciente ${id} alterado para o Status ${newStatus}`);
+  const handleClick = (newStatus, newColor) => {
+    console.log(`Paciente ${id} alterado para o Status ${newStatus}`);
     onUpdateStatus(id, newStatus);
-    }
+    setCor(newColor);
+  };
 
+  useEffect(() => {
+    localStorage.setItem(storageKey, cor);
+  }, [cor, storageKey]);
 
   return (
     <>
       <button
-        className="btn btn-primary btn-sm me-1"
-        onClick={() => handleClick(1)}
+        className={`btn ${cor} btn-sm me-1`}
+        onClick={() => handleClick(1, "btn-warning")}
         type="button"
+        disabled={cor !== "btn-primary"}
       >
-        Atender
+        {cor === "btn-primary" ? "Atender" : "Em atendimento"}
       </button>
-      <button
-        className="btn btn-success btn-sm"
-        onClick={() => handleClick(2)}
-        type="button"
-      >
-        Finalizar
-      </button>
+
+      {cor === "btn-warning" && (
+        <button
+          className="btn btn-success btn-sm"
+          onClick={() => handleClick(2, "btn-success")}
+          type="button"
+        >
+          Finalizar
+        </button>
+      )}
     </>
   );
 }
