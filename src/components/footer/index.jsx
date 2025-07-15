3ec4173352fa4b2eebd8520b170ce196e710a212
Ingridoliveira <oligrid20@gmail.com>
2025-07-15 16:11:08 -0300

    melhorarias na home e no footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,18 +1,43 @@
 function Footer() {
   return (
     <>
-      <section className="">
+      <section className="" style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}>
         <footer className="bg-body-tertiary">
-      
-
-          <div
-            className="text-center p-3"
-            style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
-          >
-            © 2020 Copyright:
-            <a className="text-body" href="https://mdbootstrap.com/">
-              MDBootstrap.com
-            </a>
+          <div className="d-flex flex-row justify-content-center align-items-center gap-4">
+            © 2025 Criadoras:
+            <h5>
+              <a
+                className="text-body"
+                target="_blank"
+                href="https://www.github.com/iingridliveira"
+              >
+                @iingridliveira
+              </a>
+            </h5>
+            <h5>
+              <a
+                className="text-body"
+                target="_blank"
+                href="https://www.github.com/xeniabia"
+              >
+                @xeniabia
+              </a>
+            </h5>
+            <h5>
+              <a
+                className="text-body"
+                target="_blank"
+                href="https://github.com/xeniabia/atendimentodepacientes?tab=readme-ov-file"
+              >
+                Repositório Git hub
+              </a>
+            </h5>
+          </div>
+          <div className="text-center py-3">
+            <p className="text-muted">
+              Atividade proposta pelo projeto Capacita Brasil Residência em Tic,
+              junto à Escola Atlântico Avanti e professor Eliakim Gama
+            </p>
           </div>
         </footer>
       </section>
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -10,75 +10,86 @@ function Home() {
         <div class="card bg-dark text-white">
           <img class="card-img" src={Img} alt="Imagem do card" />
           <div class="card-img-overlay container ">
-            <article className="p-5">
-              <h1 className="card-title fw-bold ">
-                Sistema de Triagem de Pacientes
-              </h1>
-              <p className="card-text">
-                Este é um card maior com suporte a texto embaixo, que funciona
-                como uma introdução a um conteúdo adicional. Este conteúdo é um
-                pouco maior, para demonstração.
+            <article className="p-5 justify-content-start align-items-start">
+              <h1 className="card-title fw-bold ">AVANTE TRIAGEM</h1>
+              <p className="card-text fw-bolder ">
+                Sistema de atendimento de pacientes, onde você pode cadastrar
+                pacientes, gerenciar seus níveis de prioridade e status, e
+                visualizar informações relevantes sobre cada paciente. - 🔴
+                Vermelho nível grave - 🟡 Amarelo nível brando - 🟢 Verde nível
+                leve
               </p>
-              <button className="btn btn-primary">
-                <Link to={"cadastro"} className={"nav-link"}>
-                  Cadastro
-                </Link>
-              </button>
+              <div className="container  py-5 ">
+                <div class="card-group gap-4 ">
+                  <div class="card">
+                    <img
+                      class="card-img-top"
+                      src={TV}
+                      alt="Imagem de capa do card"
+                    />
+                    <div class="card-body">
+                      <h5 class="card-title">Área de TV </h5>
+                      <p className="card-text">
+                        Neste espaço, você poderá acompanhar o andamento do seu
+                        atendimento. Aqui são exibidos os pacientes que estão
+                        sendo atendidos, os que estão aguardando na fila e os
+                        que já foram atendidos. Assim, você pode visualizar sua
+                        posição na fila e saber quando será chamado.
+                      </p>
+
+                      <Link to={"/tv"} className="btn btn-primary">
+                        TV
+                      </Link>
+                    </div>
+                  </div>
+                  <div class="card">
+                    <img
+                      class="card-img-top"
+                      src={Areamedica}
+                      alt="Imagem de capa do card"
+                    />
+                    <div class="card-body ">
+                      <h5 class="card-title">Área Médica</h5>
+                      <p className="card-text">
+                        Área Médica: neste espaço, os profissionais de saúde
+                        podem visualizar todos os pacientes cadastrados, definir
+                        o nível de prioridade de cada um e acompanhar o status
+                        do atendimento. É por aqui que médicos e equipe realizam
+                        a triagem, atualizam informações e organizam o fluxo dos
+                        atendimentos, garantindo que cada paciente seja atendido
+                        conforme sua necessidade.
+                      </p>
+
+                      <Link to={"areamedica"} className="btn btn-primary">
+                        Área Médica
+                      </Link>
+                    </div>
+                  </div>
+                  <div class="card">
+                    <img
+                      class="card-img-top"
+                      src={Cadastro}
+                      alt="Imagem de capa do card"
+                    />
+                    <div class="card-body">
+                      <h5 className="card-title">Área do Paciente</h5>
+                      <p className="card-text">
+                        Aqui você pode se cadastrar, acompanhar o andamento do
+                        seu atendimento, ver sua posição na fila e acessar
+                        informações importantes sobre seu histórico. Além disso,
+                        é possível retirar sua ficha com todos os dados do
+                        atendimento, que pode servir como comprovante ou
+                        atestado médico, facilitando o acesso às informações
+                      </p>
+
+                      <Link to={"cadastro"} className="btn btn-primary">
+                        Cadastro
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </article>
-          </div>
-        </div>
-      </section>
-      <section className="container align-items-center pb-5 ">
-        <div class="card-group ">
-          <div class="card">
-            <img class="card-img-top" src={TV} alt="Imagem de capa do card" />
-            <div class="card-body">
-              <h5 class="card-title">Título do card</h5>
-              <p class="card-text">
-                Este é um card maior com suporte a texto embaixo, que funciona
-                como uma introdução a um conteúdo adicional. Este conteúdo é um
-                pouco maior, para demonstração.
-              </p>
-              <p class="card-text">
-                <small class="text-muted">Atualizados 3 minutos atrás</small>
-              </p>
-            </div>
-          </div>
-          <div class="card">
-            <img
-              class="card-img-top"
-              src={Areamedica}
-              alt="Imagem de capa do card"
-            />
-            <div class="card-body">
-              <h5 class="card-title">Título do card</h5>
-              <p class="card-text">
-                Este é um card com suporte a texto embaixo, que funciona como
-                uma introdução a um conteúdo adicional.
-              </p>
-              <p class="card-text">
-                <small class="text-muted">Atualizados 3 minutos atrás</small>
-              </p>
-            </div>
-          </div>
-          <div class="card">
-            <img
-              class="card-img-top"
-              src={Cadastro}
-              alt="Imagem de capa do card"
-            />
-            <div class="card-body">
-              <h5 class="card-title">Título do card</h5>
-              <p class="card-text">
-                Este é um card maior com suporte a texto embaixo, que funciona
-                como uma introdução a um conteúdo adicional. Este card tem o
-                conteúdo ainda maior que o primeiro, para mostrar a altura
-                igual, em ação.
-              </p>
-              <p class="card-text">
-                <small class="text-muted">Atualizados 3 minutos atrás</small>
-              </p>
-            </div>
           </div>
         </div>
       </section>
